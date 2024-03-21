978fb666f2ce580cf69da20b31afea3ffe5cc7ac
potatopotati <122603053+potatopotati@users.noreply.github.com>
2024-03-21 19:55:19 +0800

    added routing to pages
    changes:
    Home.js = contains home page code
    Contact.js = contains contact page code
    App.js = contains routing to different pages

---
+++ lab3/sportify/src/Contact.js
@@ -0,0 +1,25 @@
+import logo from "./images/logo.png";
+import "./App.css";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Contact() {
+    return (
+        <div className="App">
+            <header>
+                <div className="logo-container">
+                    <img align="left" className="logo" src={logo} alt="logo" />
+                </div>
+                <div className="menu-container">
+                    <Link to="/" className="menu"> Home </Link>
+                    <Link to="/Contact" className="menu"> Contact Us </Link>
+                </div>
+            </header>
+            <body>
+                <h1>Contact us</h1>
+            </body>
+        </div>
+    );
+}
+
+export default Contact;
