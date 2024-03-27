204f4c756b1b24940fb7666ae10100a090af7a31
TdrDingren <97043423+TdrDingren@users.noreply.github.com>
2024-03-27 20:51:10 +0800

    make the top navigation bar into its own component

---
+++ lab3/sportify/src/components/TopNavBar/TopNavbar.js
@@ -0,0 +1,27 @@
+import logo from "../../assets/images/logo.png";
+import { Link } from "react-router-dom";
+import PopupComponent from "../Popup/PopupComponent";
+const TopNavBar = ({buttonPopup, setButtonPopup})=> {
+    return(
+        <>
+        <div className="logo-container">
+          <img align="left" className="logo" src={logo} alt="logo" />
+        </div>
+        <div className="menu-container">
+          {/* Home will lead us to homepage */}
+          <Link to="/" className="menu">
+            {" "}
+            Home{" "}
+          </Link>
+          <button className="contact-us" onClick={() => setButtonPopup(true)}>
+            Contact Us
+          </button>
+          <PopupComponent
+            buttonPopup={buttonPopup}
+            setButtonPopup={setButtonPopup}
+          />
+        </div>
+        </>
+    );
+}
+export default TopNavBar;
